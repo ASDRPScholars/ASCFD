e41e330381c535ae63c34c32f4323ddc21291e81
Jessica Li <157826160+jl61740@users.noreply.github.com>
2025-01-12 19:13:07 -0800

    fix

--- ascfd/simulation.py
+++ ascfd/simulation.py
@@ -38,9 +38,8 @@ class Simulation:
             self.bcs.apply_bcs()
             self.grid.assert_variable_type("prim")
             #get density
-            if self.inp.system == "euler2D":
-                density = self.grid.grid[self.c.RHOCOMP]
-            elif self.inp.system == "mhd2d":
+            #get density (this is only different if you have multiple types of fluids)
+            if self.inp.system == "euler2D" or self.inp.system == "mhd2d":
                 density = self.grid.grid[self.c.RHOCOMP]
             else:
                 raise RuntimeError("Density method needs to be implemented.")
