668da196af5a229b051895f2d6fb792a56cad685
Jessica Li <157826160+jl61740@users.noreply.github.com>
2025-02-22 20:40:12 -0800

    tried to implement hlld

--- ascfd/flux.py
+++ ascfd/flux.py
@@ -111,3 +111,87 @@ class Flux:
                     numFluxY_minus[icomp, i, j] = 0.5 * (fy[icomp, i, j] + fy[icomp, i, j-1]) - 0.5 * max_speed_y * (consU[icomp, i, j] - consU[icomp, i, j-1])
 
         return consU, numFluxX_plus, numFluxX_minus, numFluxY_plus, numFluxY_minus
+
+    def flux(self, U, pT, u, v, w, By, Bz, Bx):
+        rho, _, _, _, p, _, _ = U
+        
+        F = np.zeros(7)
+        F[0] = rho * u
+        F[1] = rho * u**2 + pT - Bx**2
+        F[2] = rho * u * v - Bx * By
+        F[3] = rho * u * w - Bx * Bz
+        F[4] = (p + 0.5 * (Bx**2 + By**2 + Bz**2)) * u - Bx * (u * Bx + v * By + w * Bz)
+        F[5] = By * u - Bx * v
+        F[6] = Bz * u - Bx * w
+        
+        return F
+    
+    def hlld(self, a_grid):
+        a_grid.assert_variable_type("prim")
+
+        U = a_grid.grid
+        consU = self.euler.prim_to_cons(U)
+
+        numFluxX_plus = np.zeros_like(a_grid.grid)
+        numFluxX_minus = np.zeros_like(a_grid.grid)
+        numFluxY_plus = np.zeros_like(a_grid.grid)
+        numFluxY_minus = np.zeros_like(a_grid.grid)
+
+        for i in range(a_grid.Nghost - 1, a_grid.Nx + a_grid.Nghost):
+            for j in range(a_grid.Nghost - 1, a_grid.Ny + a_grid.Nghost):
+                Bx = U[self.c.B_XCOMP, i, j]
+                U_L = U[:, i, j]
+                U_R = U[:, i+1, j]
+                
+                rho_L, u_L, v_L, w_L, p_L, Bx_L, By_L, Bz_L = U_L
+                rho_R, u_R, v_R, w_R, p_R, Bx_R, By_R, Bz_R = U_R
+                
+                pT_L = p_L + 0.5 * (Bx**2 + By_L**2 + Bz_L**2)
+                pT_R = p_R + 0.5 * (Bx**2 + By_R**2 + Bz_R**2)
+                
+                cf_L = np.sqrt((pT_L + Bx**2) / rho_L)
+                cf_R = np.sqrt((pT_R + Bx**2) / rho_R)
+                
+                S_L = min(u_L - cf_L, u_R - cf_R)
+                S_R = max(u_L + cf_L, u_R + cf_R)
+                
+                SM = ((S_R * rho_R * u_R - S_L * rho_L * u_L + pT_L - pT_R) /
+                      (S_R * rho_R - S_L * rho_L))
+                pT_star = pT_L + rho_L * (S_L - u_L) * (SM - u_L)
+                
+                rho_star_L = rho_L * (S_L - u_L) / (S_L - SM)
+                v_star_L = v_L - Bx * (By_L - By_R) / (rho_star_L * (S_L - SM))
+                w_star_L = w_L - Bx * (Bz_L - Bz_R) / (rho_star_L * (S_L - SM))
+                
+                rho_star_R = rho_R * (S_R - u_R) / (S_R - SM)
+                v_star_R = v_R - Bx * (By_R - By_L) / (rho_star_R * (S_R - SM))
+                w_star_R = w_R - Bx * (Bz_R - Bz_L) / (rho_star_R * (S_R - SM))
+                
+                S_star_L = SM - abs(Bx) / np.sqrt(rho_star_L)
+                S_star_R = SM + abs(Bx) / np.sqrt(rho_star_R)
+                
+                v_double_star_L = v_star_L + (By_L - By_R) * np.sign(Bx) / np.sqrt(rho_star_L * rho_star_R)
+                w_double_star_L = w_star_L + (Bz_L - Bz_R) * np.sign(Bx) / np.sqrt(rho_star_L * rho_star_R)
+                
+                v_double_star_R = v_star_R - (By_L - By_R) * np.sign(Bx) / np.sqrt(rho_star_L * rho_star_R)
+                w_double_star_R = w_star_R - (Bz_L - Bz_R) * np.sign(Bx) / np.sqrt(rho_star_L * rho_star_R)
+                
+                By_double_star_L = By_L * (rho_star_L / (rho_star_L + rho_star_R)) + By_R * (rho_star_R / (rho_star_L + rho_star_R))
+                Bz_double_star_L = Bz_L * (rho_star_L / (rho_star_L + rho_star_R)) + Bz_R * (rho_star_R / (rho_star_L + rho_star_R))
+                By_double_star_R = By_double_star_L
+                Bz_double_star_R = Bz_double_star_L
+                
+                if S_L > 0:
+                    F_hlld = self.flux(U_L, pT_L, u_L, v_L, w_L, By_L, Bz_L, Bx)
+                elif S_L <= 0 <= S_star_L:
+                    F_hlld = self.flux([rho_star_L, SM, v_star_L, w_star_L, pT_star, By_L, Bz_L], pT_star, SM, v_star_L, w_star_L, By_L, Bz_L, Bx)
+                elif S_star_L <= 0 <= S_star_R:
+                    F_hlld = self.flux([rho_star_R, SM, v_star_R, w_star_R, pT_star, By_R, Bz_R], pT_star, SM, v_star_R, w_star_R, By_R, Bz_R, Bx)
+                else:
+                    F_hlld = self.flux([rho_star_R, SM, v_double_star_R, w_double_star_R, pT_star, By_double_star_R, Bz_double_star_R], pT_star, SM, v_double_star_R, w_double_star_R, By_double_star_R, Bz_double_star_R, Bx)
+                
+                for icomp in range(self.c.NUMQ):
+                    numFluxX_plus[icomp, i, j] = F_hlld[icomp]
+                    numFluxX_minus[icomp, i, j] = F_hlld[icomp]
+
+        return consU, numFluxX_plus, numFluxX_minus, numFluxY_plus, numFluxY_minus
