f7603d953103ade13f5cc477f1007da390f8e0e0
matt-fang <matthewfang.calif@gmail.com>
2025-07-13 21:49:51 +0200

    rename

--- ascfd/species/params.py
+++
@@ -1,6 +0,0 @@
-class SpeciesParams:
-    def __init__(self, charge, mass, gamma, name):
-        self.charge = charge
-        self.mass = mass
-        self.gamma = gamma
-        self.name = name
