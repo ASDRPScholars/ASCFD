22bf6423786f24749cb4017122965869db0a2d49
matt-fang <matthewfang.calif@gmail.com>
2025-07-14 22:22:40 +0200

    added ParticleSpecies structure/moved from existing files: update, apply_ics, charge_density calc, etc

--- ascfd/species/particles.py
+++ ascfd/species/particles.py
@@ -1,18 +1,38 @@
 from ascfd.grid import Grid2D
 from ascfd.constants import Constants
+from ascfd.inputs import Inputs
+from ascfd.species.params import SpeciesParams
+from ascfd.fields import Fields
 
 import numpy as np
 
-
 class ParticleSpecies:
-    def __init__(self, params, n_particles):
-        self.params = params
-        self.x = np.zeros((n_particles, 2))
-        self.v = np.zeros((n_particles, 2))
+    def __init__(self, params: SpeciesParams, a_inputs: Inputs, fields: Fields):
+        self.fields = Fields(a_inputs)
         
-    def update(self):
+        self.inp = a_inputs
+        self.params = params
+        self.dt = None
+        
+        self.x = np.zeros((self.inp.n_particles, 2))
+        self.v = np.zeros((self.inp.n_particles, 2))
+        
+        
+    # TODO: ics_particles.py stuff goes in here
+    def apply_ics(self):
         pass
     
+    
+    # TODO: particles.py stuff goes in here
+    def update(self):
+        charge_density = self.get_charge_density()
+        self.fields.update_E(charge_density)
+    
+    
     def get_charge_density(self):
+        pass
+    
+    
+    def check_particles(self):
         pass
     
