c3e3ef302d866a2f5a25243021cd88d2f5531e8d
ashi1pant <ashi1pant@gmail.com>
2025-07-17 18:36:45 -0700

    particles!

--- ascfd/particle_handler.py
+++ ascfd/particle_handler.py
@@ -1,7 +1,10 @@
 import numpy as np
-
-DEFAULT_MASS = 2 # I just randomly picked these values
-DEFAULT_CHARGE = 3
+#constants
+# WHAT TO DEFAULT TO?
+DEFAULT_MASS = 2 # Ions
+DEFAULT_CHARGE = 3 #ions
+ELECTRON_MASS = 1
+ELECTRON_CHARGE = -1
 
 def create_particle(particles, position, type, charge, mass):
 
@@ -14,7 +17,17 @@ def create_particle(particles, position, type, charge, mass):
 
     particles.append(new_particle)
 
-
+def get_local_density(particles, position, type, size=1):
+    count =0
+    for p in particles:
+        if p['_type'] == type:
+            dist = np.linalg.norm(np.array(p['_position']))
+            if dist < size:
+                count+=1
+    volume=(size**3 if len(position) == 3 else size**2)
+    return count / volume if volume > 0 else 0
+def calc_ionization_rate(electron_density, neutral_density, coefficient=1e-13):
+    return coefficient*electron_density*neutral_density
 def ionize_particles(particles, ionizationRates, dt):
     neutral_particles = (particles['_type'] == "Neutral")
     neutral_particle_indices = np.where(neutral_particles)[0]
@@ -22,15 +35,14 @@ def ionize_particles(particles, ionizationRates, dt):
     for neutral_particle_index in neutral_particle_indices:
         neutral_particle = particles[neutral_particle_index]
 
-        particle_position = neutral_particle['_position']
+        position = neutral_particle['_position']
     
         # make methods to find these values depending on position (could use grid also similar to how particle cell calculation was originally done)
-        electron_density = ... 
-        neutral_density = ...
+        electron_density = get_local_density(particles, position, "Electron") 
+        neutral_density = get_local_density(particles, position, "Neutral")
+        ionization_rate = calc_ionization_rate(electron_density, neutral_density)
 
-        ionization_rate = ... # another method to calculuate
-
-        # statistics now !!!!
+        # Monte Carlo ayy
 
         p_ionize = 1 - np.exp(-ionization_rate * dt)
 
@@ -38,16 +50,13 @@ def ionize_particles(particles, ionizationRates, dt):
 
         if rand > p_ionize: # quick exit case: we will NOT ionize
             continue
-    
+            # should this + what follows be an else statement?
+            # where id neutral_particle defined
         particles.pop(neutral_particle_index)
 
         # create new ion in its place with correct values etc...
+        # defaulted the charge and mass to 1?
+        create_particle(particles, position, "Ion", DEFAULT_CHARGE, DEFAULT_MASS)
+        create_particle(particles, position, "Electron", ELECTRON_CHARGE, ELECTRON_MASS)
 
-        # create_particle()
-
-
-        
-        
-
-
-
+def update_velocity(particle, charge, electric_field, magnetic_field, dt):
