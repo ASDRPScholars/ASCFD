b296c41dc8ffa90b170d2b801e8c6f7279cc1e3e
Matthew Fang <matthewfang.calif@gmail.com>
2025-08-01 18:55:19 -0700

    GREAT - b not quite there yet but density trend looking very good (and maybe even E too btu we just can't see E density yet)

--- ascfd/fields/ics.py
+++ ascfd/fields/ics.py
@@ -25,8 +25,8 @@ class FieldInitialConditions:
     def guillaume(self):
         ic_grid = np.zeros_like(self.B_field)
         
-        B_max = 0.2 # 0.237 -> 0.02 per claude
-        x_c = 0.5
+        B_max = 20 # 0.237 -> 0.02 per claude
+        x_c = 0.7
         sigma = 0.05
         
         x = np.linspace(self.inp.xlim[0], self.inp.xlim[1], self.inp.nx)
