added appropriate dt + fixed flipped scaling but pressure still broken

ascfd/fluid/species.py
@@ -271,19 +271,19 @@ class FluidSpecies:
     def get_number_density(self):
         number_density = self.grid[self.c.RHOCOMP] / self.params.mass
         return number_density
-    
+
     
     def normal_to_phys(self):
         grid = copy.copy(self.grid)
         ref = self.ref
         
         grid[self.c.RHOCOMP] = grid[self.c.RHOCOMP] * (ref.m / ref.L**3)
-        grid[self.c.UCOMP] = grid[self.c.UCOMP] * (ref.dt / ref.L)
-        grid[self.c.VCOMP] = grid[self.c.VCOMP] * (ref.dt / ref.L)
-        grid[self.c.VCOMP] = grid[self.c.VCOMP] * (ref.dt / ref.L)
+        grid[self.c.MUCOMP] = grid[self.c.MUCOMP] * (ref.L / ref.dt)
+        grid[self.c.MVCOMP] = grid[self.c.MVCOMP] * (ref.L / ref.dt)
+        grid[self.c.ECOMP] = grid[self.c.ECOMP] * ((ref.m * ref.L ** 2) / ref.dt ** 2)
         
-        np.set_printoptions(threshold=sys.maxsize)
-        print(grid[self.c.RHOCOMP])
+        # np.set_printoptions(threshold=sys.maxsize)
+        # print(grid[self.c.RHOCOMP])
         return grid
     
     