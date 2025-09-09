d952f791c8058621612d99cc6137da0fc702cc07
matt-fang <matthewfang.calif@gmail.com>
2025-09-09 16:33:53 -0700

    FIX ref.v TO BE CONSISTENT WITH X AND DT LOL

--- ascfd/plasma_refs.py
+++ ascfd/plasma_refs.py
@@ -15,7 +15,7 @@ class PlasmaReferences:
         
         # TODO: SHOULD WE KEEP THIS @ MORE STABLE 1?
         # TODO: IS THIS THE RIGHT RANGE TO BE NORMALIZING IN?? DO WE PICK ANOTHER ref.dt?? DO WE NORMALIZE DT AT ALL??
-        self.v = 2.18e-16 # 1.0
+        self.v = 2.18e6 # 1.0
         self.dt = 1e-9
     
     # def __init__(self, n0=1e18, T0=1000.0, species_mass=9.1e-31, species_charge=1.6e-19):
