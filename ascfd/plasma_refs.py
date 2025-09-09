0d5eeca87ec8a2960dceac39f70024cc7ca0f3d2
matt-fang <matthewfang.calif@gmail.com>
2025-09-08 22:36:50 -0700

    changed ref.v to be consistent with ref.m and ref.dt - idk if this is right move - sim is more much more dynamic

--- ascfd/plasma_refs.py
+++ ascfd/plasma_refs.py
@@ -7,10 +7,15 @@ class PlasmaReferences:
     """
     
     def __init__(self):
+        # TODO: IS IT BETTER TO NORMALIZE ACCORDING TO BULK VALUES?
+        
         self.m = 2.18e-25
         self.q = 1.602e-19
         self.L = 0.001
-        self.v = 1.00
+        
+        # TODO: SHOULD WE KEEP THIS @ MORE STABLE 1?
+        # TODO: IS THIS THE RIGHT RANGE TO BE NORMALIZING IN?? DO WE PICK ANOTHER ref.dt?? DO WE NORMALIZE DT AT ALL??
+        self.v = 2.18e-16 # 1.0
         self.dt = 1e-9
     
     # def __init__(self, n0=1e18, T0=1000.0, species_mass=9.1e-31, species_charge=1.6e-19):
