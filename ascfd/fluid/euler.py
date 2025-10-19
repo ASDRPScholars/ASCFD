30f03ee43b2e171ae139d26f8f1883d41dc69aea
matt-fang <matthewfang.calif@gmail.com>
2025-10-19 12:41:46 -0700

    correctly (?) applied energy->temp flux?

--- ascfd/fluid/euler.py
+++ ascfd/fluid/euler.py
@@ -174,7 +174,7 @@ class FluidEuler:
             # print("!@! 5/2*j_e*k_B*T_e - Q_e_perp", energy_flux)
             
             
-            flux_x[self.c.TCOMP] = 2/(3*n_e*k_B) # thermal energy density (3/2 * n_e * k_B * T_e) equation - rearrange mikellides eq (25), or marks eq (4.8)
+            flux_x[self.c.TCOMP] = 2/(3*n_e*k_B) * energy_flux # thermal energy density (3/2 * n_e * k_B * T_e) equation - rearrange mikellides eq (25), or marks eq (4.8)
             # TODO: flux_y[self.c.ECOMP] = ...
             
 
