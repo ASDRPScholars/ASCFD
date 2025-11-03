d6696133ec1acc1c4f04ca314cc115111411f961
matt-fang <matthewfang.calif@gmail.com>
2025-11-03 15:32:23 -0800

    left thermal flux friendly enough

--- ascfd/fluid/species.py
+++ ascfd/fluid/species.py
@@ -51,14 +51,73 @@ class FluidSpecies:
         
         
     def update(self):
-        
-        print("dt is", self.dt)
+        ng = self.inp.ng
+        k_B = self.c.k_B
+        m_e = self.inp.m_e
         consU = self.euler.prim_to_cons(self.grid)
 
         _, right_flux, left_flux, top_flux, bottom_flux = self.flux.getFlux(self.grid, self.inp.nx, self.inp.ny, self.inp.ng)
                 
-        for i in range(self.inp.ng, self.inp.nx + self.inp.ng):
-            for j in range(self.inp.ng, self.inp.ny + self.inp.ng):
+        ### THERMAL SHEATH BOUNDARIES
+        
+        T_e = 2/(3 * k_B) * (consU[self.c.ECOMP] - (1/2 * m_e * consU[self.c.UCOMP]**2))
+        v_th = np.sqrt(2 * k_B * T_e / m_e)
+        
+        phi_th = consU[self.c.RHOCOMP] * v_th / (2 * np.sqrt(np.pi))
+        Q_th = consU[self.c.RHOCOMP] * v_th / (2 * np.sqrt(np.pi)) * (2 * k_B * T_e) / (m_e)
+        
+        left_convect_flux = left_flux[self.c.RHOCOMP, ng, :]
+        left_outflow_mask = left_convect_flux > 0
+        
+        left_flux[self.c.RHOCOMP, ng, :] = np.where(
+            left_outflow_mask,
+            left_convect_flux + phi_th[ng, :],  # Add thermal mass flux
+            0.0  # Block inflow
+        )
+
+        left_flux[self.c.ECOMP, ng, :] = np.where(
+            left_outflow_mask,
+            left_flux[self.c.ECOMP, ng, :] + Q_th[ng, :],  # Add thermal energy flux
+            0.0  # Block inflow
+        )
+        
+        # # TOP
+        
+        # top_convect_flux = top_flux[self.c.RHOCOMP, :, ng]
+        # top_outflow_mask = top_convect_flux < 0
+        
+        # top_flux[self.c.RHOCOMP, :, ng] = np.where(
+        #     top_outflow_mask,
+        #     top_convect_flux + phi_th[:, ng],  # Add thermal mass flux
+        #     0.0  # Block inflow
+        # )
+
+        # top_flux[self.c.ECOMP, :, ng] = np.where(
+        #     top_outflow_mask,
+        #     top_flux[self.c.ECOMP, :, ng] + Q_th[:, ng],  # Add thermal energy flux
+        #     0.0  # Block inflow
+        # )
+        
+        # # BOTTOM
+        
+        # bottom_convect_flux = bottom_flux[self.c.RHOCOMP, :, self.inp.ny + ng]
+        # bottom_outflow_mask = bottom_convect_flux < 0
+        
+        # bottom_flux[self.c.RHOCOMP, :, self.inp.ny + ng] = np.where(
+        #     bottom_outflow_mask,
+        #     bottom_convect_flux + phi_th[:, self.inp.ny + ng],  # Add thermal mass flux
+        #     0.0  # Block inflow
+        # )
+
+        # bottom_flux[self.c.ECOMP, :, self.inp.ny + ng] = np.where(
+        #     bottom_outflow_mask,
+        #     bottom_flux[self.c.ECOMP, :, self.inp.ny + ng] + Q_th[:, self.inp.ny + ng],  # Add thermal energy flux
+        #     0.0  # Block inflow
+        # )
+
+        
+        for i in range(ng, self.inp.nx + ng):
+            for j in range(ng, self.inp.ny + ng):
                 for icomp in range(self.c.NUMQ):
                     
                     delta = (
